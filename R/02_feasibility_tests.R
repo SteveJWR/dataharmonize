0d52871fd6b38a5e049868d42183c83b35403546
SteveJWR <steven.wilkins.reeves@gmail.com>
2023-05-12 09:13:44 -0700

    Update the Feasibility Test Parameters

--- R/02_feasibility_tests.R
+++ R/02_feasibility_tests.R
@@ -26,7 +26,7 @@ if(id %% 2 == 1){
 R.bins = 1000
 
 # True Conditional Model
-h.true <- 2
+h.true <- 3
 
 N <- 30
 
@@ -34,16 +34,16 @@ cond.true <- conditional_mkm(N,ker.true, h.true)
 
 
 two.obs.ratio = 1
-n.seq = c(100,200,500,1000)
+n.seq = c(100,500,1000,5000, 10000)
 
 J = length(n.seq)
 
 
 # grid for the values of h
-h.set <- c(0.5,0.8,1,2,3,5,10,20)
+h.set <- c(0.8,1,2,3,5,10,20)
 H = length(h.set)
 
-i.true = 3
+
 if(kernel == "Gaussian"){
   cond.set <- generate_mkm_list(N = N, ker = gaussian_kernel, h.set = h.set)
   cond.names <- paste0("Gaussian h = ",as.character(h.set))
@@ -54,7 +54,6 @@ if(kernel == "Gaussian"){
 
 
 
-# correct model selection fraction of sims.
 n.sims <- 5
 res1 <- array(NA, c(n.sims,length(n.seq),H))
 res2 <- res1
@@ -97,6 +96,122 @@ for(j in seq(J)){
 
 saveRDS(res1, paste0("data/feasibility_test_order_1_results_",kernel,ceiling(id/2), ".rds"))
 saveRDS(res2, paste0("data/feasibility_test_order_2_results_",kernel,ceiling(id/2), ".rds"))
+
+
+
+
+
+
+
+
+make.plots = F
+
+if(make.plots){
+  library(ggpubr)
+  png.width = 1200
+  png.height = 1000
+  png.res = 200
+  n.seq = c(100,500,1000,5000, 10000)
+
+  alpha = 0.05 #
+
+  # grid for the values of h
+  h.set <- c(0.8,1,2,3,5,10,20)
+
+  kernel <- "Gaussian" #"Exponential" #"Gaussian"
+  # update this section to concatenate the results
+  res1 <- readRDS(paste0("data/feasibility_test_order_1_results_",kernel,1, ".rds"))
+  res2 <- readRDS(paste0("data/feasibility_test_order_2_results_",kernel,1, ".rds"))
+
+
+  for(j in seq(2,100)){
+
+    res1.tmp <- readRDS(paste0("data/feasibility_test_order_1_results_",kernel,j, ".rds"))
+    res2.tmp <- readRDS(paste0("data/feasibility_test_order_2_results_",kernel,j, ".rds"))
+
+    res1 <- abind(res1, res1.tmp, along = 1)
+    res2 <- abind(res2, res2.tmp, along = 1)
+
+  }
+
+
+  n.sims = dim(res1)[1]
+  num.samples = dim(res1)[2]
+  H = dim(res1)[3]
+
+  samp.size.vec = rep(paste0("n = ", n.seq), each = H)
+  p.vec <- c() # rejection probabilities
+  for(j in seq(num.samples)){
+    p.vec <- c(p.vec, colMeans(as.matrix(res1[,j,] < alpha), na.rm = T))
+  }
+
+  se.vec <- sqrt(p.vec*(1 - p.vec)/n.sims)
+  res.data <- data.frame("SampleSize" = samp.size.vec,
+                         "Bandwidth" = rep(h.set, times = num.samples),
+                         "rejectProb" = p.vec,
+                         "rejectProb_sd" = se.vec)
+
+  res.data$SampleSize <- factor(res.data$SampleSize, levels = paste0("n = ", n.seq))
+  plt.1 <- ggplot(res.data, aes(x = Bandwidth, y = rejectProb, color = SampleSize)) +
+    geom_line()  +
+    geom_ribbon(aes(ymin = rejectProb - 2*rejectProb_sd, ymax = rejectProb + 2*rejectProb_sd, fill = SampleSize),alpha=0.3) +
+    ggtitle(paste0(kernel, " Kernel First Order Feasibility Test")) +
+    xlab("Bandwidth (h)") +
+    ylab("Rejection Probability")
+
+  #geom_line(aes(x = n, y = rmse, color = method)) #+
+  #geom_errorbar(aes(ymin = bias - 2*rmse, ymax = bias + 2*rmse))
+
+  plt.1
+
+  png(filename = paste0("plots/feasibility_test",kernel,".png"),
+      width = png.width, height = png.height, res = png.res)
+
+  plt.1
+  # Close the pdf file
+  dev.off()
+
+
+
+
+  n.sims = dim(res2)[1]
+  num.samples = dim(res2)[2]
+  H = dim(res2)[3]
+
+  samp.size.vec = rep(paste0("n = ", n.seq), each = H)
+  p.vec <- c() # rejection probabilities
+  for(j in seq(num.samples)){
+    p.vec <- c(p.vec, colMeans(as.matrix(res2[,j,] < alpha), na.rm = T))
+  }
+
+  se.vec <- sqrt(p.vec*(1 - p.vec)/n.sims)
+  res.data <- data.frame("SampleSize" = samp.size.vec,
+                         "Bandwidth" = rep(h.set, times = num.samples),
+                         "rejectProb" = p.vec,
+                         "rejectProb_sd" = se.vec)
+
+  res.data$SampleSize <- factor(res.data$SampleSize, levels = paste0("n = ", n.seq))
+  plt.2 <- ggplot(res.data, aes(x = log(Bandwidth), y = rejectProb, color = SampleSize)) +
+    geom_line()  +
+    geom_ribbon(aes(ymin = rejectProb - 2*rejectProb_sd, ymax = rejectProb + 2*rejectProb_sd, fill = SampleSize),alpha=0.3) +
+    ggtitle(paste0(kernel, " Kernel First Order Feasibility Test")) +
+    xlab("log-Bandwidth (h)") +
+    ylab("Rejection Probability")
+
+  #geom_line(aes(x = n, y = rmse, color = method)) #+
+  #geom_errorbar(aes(ymin = bias - 2*rmse, ymax = bias + 2*rmse))
+
+  plt.2
+
+  png(filename = paste0("plots/feasibility_test",kernel,".png"),
+      width = png.width, height = png.height, res = png.res)
+
+  plt.1
+  # Close the pdf file
+  dev.off()
+
+
+}
 
 
 
